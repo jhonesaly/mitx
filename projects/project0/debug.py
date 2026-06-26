2f8faf2a136032a39d45baebff3e664c32d84506
Alyson Jhones de Souza Andrade <alyson.andrade@serpro.gov.br>
2026-06-26 11:08:20 -0300

    feat: init project 2

--- projects/project0/debug.py
+++ projects/project0/debug.py
@@ -1,6 +1,11 @@
-def get_sum_metrics(predictions, metrics=[]):
+def get_sum_metrics(predictions, metrics=None):
+    if metrics is None:
+        metrics = []
+    else:
+        metrics = list(metrics)
+
     for i in range(3):
-        metrics.append(lambda x: x + i)
+        metrics.append(lambda x, i=i: x + i)
 
     sum_metrics = 0
     for metric in metrics:
