fdd8b83cf70c4bf6a441af1b57d04a9ec0a06c8c
Alyson Jhones de Souza Andrade <alyson.andrade@serpro.gov.br>
2026-06-26 11:09:09 -0300

    feat: closed_form

--- projects/project2/part1/linear_regression.py
+++ projects/project2/part1/linear_regression.py
@@ -5,18 +5,14 @@ import numpy as np
 def closed_form(X, Y, lambda_factor):
     """
     Computes the closed form solution of linear regression with L2 regularization
-
-    Args:
-        X - (n, d + 1) NumPy array (n datapoints each with d features plus the bias feature in the first dimension)
-        Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
-            data point
-        lambda_factor - the regularization constant (scalar)
-    Returns:
-        theta - (d + 1, ) NumPy array containing the weights of linear regression. Note that theta[0]
-        represents the y-axis intercept of the model and therefore X[0] = 1
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    # Cria a matriz identidade com a mesma dimensão do número de features (d + 1)
+    I = np.eye(X.shape[1])
+    
+    # Calcula theta usando a fórmula de forma fechada
+    theta = np.linalg.inv(X.T @ X + lambda_factor * I) @ X.T @ Y
+    
+    return theta
 
 ### Functions which are already complete, for you to use ###
 
