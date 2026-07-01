4547ad769165a2eb1016dcdbcc37b92b771eb227
Alyson Jhones de Souza Andrade <alyson.andrade@serpro.gov.br>
2026-07-01 13:58:38 -0300

    feat: implement MNIST classification pipelines for regression, SVM, and softmax models

--- projects/project2/part1/main.py
+++ projects/project2/part1/main.py
@@ -119,6 +119,9 @@ def run_softmax_on_MNIST(temp_parameter=1):
 
     # TODO: add your code here for the "Using the Current Model" question in tab 6.
     #      and print the test_error_mod3
+    train_y_mod3, test_y_mod3 = update_y(train_y, test_y)
+    test_error_mod3 = compute_test_error_mod3(test_x, test_y_mod3, theta, temp_parameter)
+    print(f'softmax test_error_mod3 (temp={temp_parameter}) = {test_error_mod3}')
     return test_error
 
 
@@ -140,11 +143,15 @@ def run_softmax_on_MNIST_mod3(temp_parameter=1):
 
     See run_softmax_on_MNIST for more info.
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    train_x, train_y, test_x, test_y = get_MNIST_data()
+    train_y_mod3, test_y_mod3 = update_y(train_y, test_y)
+    theta, cost_function_history = softmax_regression(train_x, train_y_mod3, temp_parameter, alpha=0.3, lambda_factor=1.0e-4, k=3, num_iterations=150)
+    test_error = compute_test_error(test_x, test_y_mod3, theta, temp_parameter)
+    return test_error
 
 
 # TODO: Run run_softmax_on_MNIST_mod3(), report the error rate
+print('softmax mod3 test_error =', run_softmax_on_MNIST_mod3(temp_parameter=1))
 
 
 #######################################################################
