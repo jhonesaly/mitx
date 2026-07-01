9cc576b015e7a6f57699384983d0504f1c562b2b
Alyson Jhones de Souza Andrade <alyson.andrade@serpro.gov.br>
2026-07-01 08:23:18 -0300

    feat: error calc

--- projects/project2/part1/svm.py
+++ projects/project2/part1/svm.py
@@ -7,15 +7,17 @@ from sklearn.svm import LinearSVC
 def one_vs_rest_svm(train_x, train_y, test_x):
     """
     Trains a linear SVM for binary classifciation
-
-    Args:
-        train_x - (n, d) NumPy array (n datapoints each with d features)
-        train_y - (n, ) NumPy array containing the labels (0 or 1) for each training data point
-        test_x - (m, d) NumPy array (m datapoints each with d features)
-    Returns:
-        pred_test_y - (m,) NumPy array containing the labels (0 or 1) for each test data point
     """
-    raise NotImplementedError
+    # Inicializa o modelo com os parâmetros exigidos
+    svm_model = LinearSVC(random_state=0, C=0.1)
+    
+    # Treina o modelo com os dados fornecidos
+    svm_model.fit(train_x, train_y)
+    
+    # Realiza a predição nos dados de teste e retorna o resultado
+    pred_test_y = svm_model.predict(test_x)
+    
+    return pred_test_y
 
 
 def multi_class_svm(train_x, train_y, test_x):
