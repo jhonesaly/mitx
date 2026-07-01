04543c75fd09c94c76ada11625eff238476f232b
Alyson Jhones de Souza Andrade <alyson.andrade@serpro.gov.br>
2026-07-01 10:21:45 -0300

    feat: multi_class_svm

--- projects/project2/part1/svm.py
+++ projects/project2/part1/svm.py
@@ -21,17 +21,11 @@ def one_vs_rest_svm(train_x, train_y, test_x):
 
 
 def multi_class_svm(train_x, train_y, test_x):
-    """
-    Trains a linear SVM for multiclass classifciation using a one-vs-rest strategy
-
-    Args:
-        train_x - (n, d) NumPy array (n datapoints each with d features)
-        train_y - (n, ) NumPy array containing the labels (int) for each training data point
-        test_x - (m, d) NumPy array (m datapoints each with d features)
-    Returns:
-        pred_test_y - (m,) NumPy array containing the labels (int) for each test data point
-    """
-    raise NotImplementedError
+    svm_model = LinearSVC(random_state=0, C=0.1)
+    svm_model.fit(train_x, train_y)
+    pred_test_y = svm_model.predict(test_x)
+    
+    return pred_test_y
 
 
 def compute_test_error_svm(test_y, pred_test_y):
