899f245f81b6dae428796746dfdf9a3c21ac8516
Alyson Jhones de Souza Andrade <alyson.andrade@serpro.gov.br>
2026-07-04 07:13:03 -0300

    feat: encerramento da part1

--- projects/project2/part1/main.py
+++ projects/project2/part1/main.py
@@ -45,9 +45,9 @@ def run_linear_regression_on_MNIST(lambda_factor=1):
 
 
 # Don't run this until the relevant functions in linear_regression.py have been fully implemented.
-print('Linear Regression test_error lambda=1 =', run_linear_regression_on_MNIST(lambda_factor=1))
-print('Linear Regression test_error lambda=0.1 =', run_linear_regression_on_MNIST(lambda_factor=0.1))
-print('Linear Regression test_error lambda=0.01 =', run_linear_regression_on_MNIST(lambda_factor=0.01))
+#print('Linear Regression test_error lambda=1 =', run_linear_regression_on_MNIST(lambda_factor=1))
+#print('Linear Regression test_error lambda=0.1 =', run_linear_regression_on_MNIST(lambda_factor=0.1))
+#print('Linear Regression test_error lambda=0.01 =', run_linear_regression_on_MNIST(lambda_factor=0.01))
 
 #######################################################################
 # 3. Support Vector Machine
@@ -70,7 +70,7 @@ def run_svm_one_vs_rest_on_MNIST():
     return test_error
 
 
-print('SVM one vs. rest test_error:', run_svm_one_vs_rest_on_MNIST())
+#print('SVM one vs. rest test_error:', run_svm_one_vs_rest_on_MNIST())
 
 
 def run_multiclass_svm_on_MNIST():
@@ -86,7 +86,7 @@ def run_multiclass_svm_on_MNIST():
     return test_error
 
 
-print('Multiclass SVM test_error:', run_multiclass_svm_on_MNIST())
+#print('Multiclass SVM test_error:', run_multiclass_svm_on_MNIST())
 
 #######################################################################
 # 4. Multinomial (Softmax) Regression and Gradient Descent
@@ -125,11 +125,11 @@ def run_softmax_on_MNIST(temp_parameter=1):
     return test_error
 
 
-for temp_parameter in [0.5, 1.0, 2.0]:
-    print(f'softmax test_error (temp={temp_parameter}) =', run_softmax_on_MNIST(temp_parameter=temp_parameter))
+#for temp_parameter in [0.5, 1.0, 2.0]:
+    #print(f'softmax test_error (temp={temp_parameter}) =', run_softmax_on_MNIST(temp_parameter=temp_parameter))
 
 # Re-run for temp_parameter=1 to ensure the final saved theta corresponds to it
-run_softmax_on_MNIST(temp_parameter=1)
+#run_softmax_on_MNIST(temp_parameter=1)
 
 #######################################################################
 # 6. Changing Labels
@@ -151,7 +151,7 @@ def run_softmax_on_MNIST_mod3(temp_parameter=1):
 
 
 # TODO: Run run_softmax_on_MNIST_mod3(), report the error rate
-print('softmax mod3 test_error =', run_softmax_on_MNIST_mod3(temp_parameter=1))
+##print('softmax mod3 test_error =', run_softmax_on_MNIST_mod3(temp_parameter=1))
 
 
 #######################################################################
@@ -177,6 +177,9 @@ test_pca = project_onto_PC(test_x, pcs, n_components, feature_means)
 
 # TODO: Train your softmax regression model using (train_pca, train_y)
 #       and evaluate its accuracy on (test_pca, test_y).
+theta, cost_function_history = softmax_regression(train_pca, train_y, temp_parameter=1, alpha=0.3, lambda_factor=1.0e-4, k=10, num_iterations=150)
+test_error = compute_test_error(test_pca, test_y, theta, temp_parameter=1)
+print(f'softmax test_error com PCA 18 componentes (temp=1) = {test_error}')
 
 
 # TODO: Use the plot_PC function in features.py to produce scatterplot
@@ -190,28 +193,41 @@ plot_PC(train_x[range(000, 100), ], pcs, train_y[range(000, 100)], feature_means
 #       their 18-dimensional principal component representation.
 #       Compare the reconstructed images with the originals.
 firstimage_reconstructed = reconstruct_PC(train_pca[0, ], pcs, n_components, train_x, feature_means)#feature_means added since release
-plot_images(firstimage_reconstructed)
-plot_images(train_x[0, ])
+#plot_images(firstimage_reconstructed)
+#plot_images(train_x[0, ])
 
 secondimage_reconstructed = reconstruct_PC(train_pca[1, ], pcs, n_components, train_x, feature_means)#feature_means added since release
-plot_images(secondimage_reconstructed)
-plot_images(train_x[1, ])
+#plot_images(secondimage_reconstructed)
+#plot_images(train_x[1, ])
 
 
 ## Cubic Kernel ##
 # TODO: Find the 10-dimensional PCA representation of the training and test set
-
+n_components_10 = 10
+train_pca10 = project_onto_PC(train_x, pcs, n_components_10, feature_means)
+test_pca10 = project_onto_PC(test_x, pcs, n_components_10, feature_means)
 
 # TODO: First fill out cubicFeatures() function in features.py as the below code requires it.
-
 train_cube = cubic_features(train_pca10)
 test_cube = cubic_features(test_pca10)
-# train_cube (and test_cube) is a representation of our training (and test) data
-# after applying the cubic kernel feature mapping to the 10-dimensional PCA representations.
-
 
 # TODO: Train your softmax regression model using (train_cube, train_y)
 #       and evaluate its accuracy on (test_cube, test_y).
+theta_cube, cost_function_history_cube = softmax_regression(train_cube, train_y, temp_parameter=1, alpha=0.3, lambda_factor=1.0e-4, k=10, num_iterations=150)
+test_error_cube = compute_test_error(test_cube, test_y, theta_cube, temp_parameter=1)
+print(f'Error rate for 10-dimensional cubic PCA features = {test_error_cube}')
 
-if __name__ == "__main__":
-    print("ainda estou aqui.")
+# Polynomial svm using scikit-learn
+from sklearn.svm import SVC
+poly_svm = SVC(random_state=0, kernel='poly', degree=3)
+poly_svm.fit(train_pca10, train_y)
+pred_test_y_poly = poly_svm.predict(test_pca10)
+test_error_poly = compute_test_error_svm(test_y, pred_test_y_poly)
+print(f'Error rate for 10-dimensional PCA features using cubic polynomial svm = {test_error_poly}')
+
+# Rbf svm using scikit-learn
+rbf_svm = SVC(random_state=0, kernel='rbf')
+rbf_svm.fit(train_pca10, train_y)
+pred_test_y_rbf = rbf_svm.predict(test_pca10)
+test_error_rbf = compute_test_error_svm(test_y, pred_test_y_rbf)
+print(f'Error rate for 10-dimensional PCA features using rbf svm = {test_error_rbf}')
